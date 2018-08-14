c8dbbca71030e5dac17ecb6b759dcabc530f7158
Marnie Paraiso <marnie.paraiso@paramount.com.ph>
2018-08-14 11:09:00 +0800

    change password function with required authorization header token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    decoded_token = JsonWebToken.decode(request.headers["Authorization"])
+    unless decoded_token == "Invalid Access Token."
+      decoded_token[0]
+    else
+      decoded_token
+    end
+  end
+
 end
--- app/graphql/resolvers/change_password.rb
+++ app/graphql/resolvers/change_password.rb
@@ -1,43 +1,32 @@
 class Resolvers::ChangePassword < GraphQL::Function
-  argument :token, !types.String
   argument :email_address, !types.String
   argument :password, !types.String
 
   type do
     name 'ChangePassword'
 
-    field :password, types.String
+    field :message, types.String
   end
 
   def call(_obj, args, _ctx)
-    pass = args[:password]
-    dtoken = JsonWebToken.decode(args[:token])
-
-    if dtoken != 'Invalid Access Token.'
+    message = _ctx[:current_user]
+    if message != 'Invalid Access Token.'
       user = User.where(email_address: args[:email_address]).first
 
       if user.nil?
-        pass = 'Record not found!'
+        message = 'Record not found!'
       else
-        user.update(password: encryptpass(pass))
+        user.update(password: encryptpass(args[:password]))
+        message = 'Password successfully changed!'
       end
-
-    else
-      pass = 'Invalid Access Token.'
     end
 
     OpenStruct.new({
-      password: pass
+      message: message
     })
-
   end
 
   def encryptpass(val)
     Digest::MD5.hexdigest(val)
   end
-
-  def stringfy_json(val, col)
-    val.to_s[/\"#{col}"\=>?\"([a-zA-Z ]+)/].gsub(/\"#{col}"\=>?\"/, '')
-  end
-
 end
