df9015a7cb8ffe2ad8581e5ec8078abf4205b6aa
Marnie Paraiso <marnie.paraiso@paramount.com.ph>
2018-08-16 10:12:58 +0800

    create agent - with access token

--- app/graphql/resolvers/create_agent.rb
+++ app/graphql/resolvers/create_agent.rb
@@ -2,12 +2,24 @@ class Resolvers::CreateAgent < GraphQL::Function
   argument :cde, !types.String
   argument :fullname, !types.String
 
-  type Types::AgentType
+  type do
+    name 'CreateAgent'
+
+    field :message, types.String
+  end
 
   def call(_obj, args, _ctx)
-    Agent.create!(
-      cde: args[:cde],
-      fullname: args[:fullname]
-    )
+    message = _ctx[:current_user]
+    if message != 'Invalid Access Token.'
+      Agent.create!(
+        cde: args[:cde],
+        fullname: args[:fullname]
+      )
+      message = 'Agent successfully added!'
+    end
+
+    OpenStruct.new({
+      message: message
+    })
   end
 end
