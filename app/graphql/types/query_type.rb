5daefb1c7ba9d5d5bb9eb8ca7eb7da7fd57de030
Marnie Paraiso <marnie.paraiso@paramount.com.ph>
2018-08-20 14:09:22 +0800

    add roles and user(arg id) query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,4 +2,21 @@ Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
 
   field :myProfile, function: Resolvers::MyProfile.new
+
+  field :roles, types[Types::RoleType] do
+    resolve ->(_obj, _args, ctx) {
+      if ctx[:current_user] != 'Invalid Access Token.'
+        Role.all
+      end
+    }
+  end
+
+  field :user, Types::UserType do
+    argument :id, !types.ID
+    resolve ->(_obj, args, ctx) {
+      if ctx[:current_user] != 'Invalid Access Token.'
+        User.where(id: args[:id]).first
+      end
+    }
+  end
 end
